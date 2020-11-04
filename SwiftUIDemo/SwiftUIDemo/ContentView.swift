41192b71e10b61378ff647013f06ffbab9e6e5f1
Chen Chen <a950727b@gmail.com>
2020-11-04 09:02:02 -0800

    updated

--- SwiftUIDemo/SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -7,27 +7,251 @@
 //
 
 import SwiftUI
-let urlStr = "https://jsonplaceholder.typicode.com/todos/"
+
 struct ContentView: View {
-    @State private var results = [UserInfo]()
-    func getData() {
-        let url = URL(string: urlStr)!
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let data = data {
-                self.results = try! JSONDecoder.init().decode([UserInfo].self, from: data)
-            }
-        }.resume()
-    }
-    
     var body: some View {
-        List(results, id: \.id)  {
-            Text($0.title)
-        }.onAppear(perform: getData)
+        
+        TabView{
+            
+            Home().tabItem {
+                
+                Image("home").font(.title)
+            }
+            
+            Text("Activity").tabItem {
+                
+                Image("activity").font(.title)
+            }
+            
+            Text("Search").tabItem {
+                
+                Image("search").font(.title)
+            }
+            
+            Text("Setting").tabItem {
+                
+                Image("setting").font(.title)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home : View {
+    
+    var body : some View{
+        
+        VStack(alignment: .leading,spacing: 12){
+            
+            HStack{
+                
+                Button(action: {
+                    
+                }) {
+                    
+                    Image("menu").renderingMode(.original)
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                }) {
+                    
+                    Image("profile_icon").renderingMode(.original)
+                }
+            }
+            
+            Text("Explore").fontWeight(.heavy).font(.largeTitle).padding(.top,15)
+            
+            HStack{
+                
+                Button(action: {
+                    
+                }) {
+                    
+                    Text("Experiences").fontWeight(.heavy).foregroundColor(.black)
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                }) {
+                    
+                    Text("Restaurants").foregroundColor(.gray)
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                }) {
+                    
+                    Text("Hotels").foregroundColor(.gray)
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                }) {
+                    
+                    Text("Tickets").foregroundColor(.gray)
+                }
+            }.padding([.top],30)
+            .padding(.bottom, 15)
+        
+            CategoryView()
+            
+            ActivityView().padding(.top, 10)
+            
+        }.padding()
+    }
+}
+
+struct CategoryView : View {
+    
+    @State var show = false
+    
+    var body : some View{
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+            
+            
+            HStack(spacing: 20){
+                
+                VStack(alignment: .leading,spacing: 12){
+                    
+                    Button(action: {
+                        
+                    }) {
+                        
+                        Image("Card1").renderingMode(.original)
+                    }
+                    
+                    Text("Stream Fishing").fontWeight(.heavy)
+                    
+                    HStack(spacing: 5){
+                        
+                        Image("map").renderingMode(.original)
+                        Text("Walnut Creek, CA").foregroundColor(.gray)
+                    }
+                }
+                
+                VStack(alignment: .leading,spacing: 12){
+                    
+                    Button(action: {
+                        
+                        self.show.toggle()
+                        
+                    }) {
+                        
+                        Image("Card2").renderingMode(.original)
+                    }
+                    
+                    Text("Mountain Camping").fontWeight(.heavy)
+                    
+                    HStack(spacing: 5){
+                        
+                        Image("map").renderingMode(.original)
+                        Text("Yosemite, CA").foregroundColor(.gray)
+                    }
+                }
+            }
+            
+        }.sheet(isPresented: $show) {
+            
+            DetailView()
+        }
+    }
+}
+
+
+struct ActivityView : View {
+    
+    var body : some View{
+        
+        VStack{
+            
+            HStack{
+                
+                Text("Equipment rental").fontWeight(.heavy)
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                }) {
+                    
+                    Text("View all").foregroundColor(.gray)
+                }
+                
+            }.padding([.top], 15)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                
+                HStack(spacing: 35){
+                    
+                    Button(action: {
+                            
+                    }) {
+                            
+                        VStack(spacing: 8){
+                                
+                            Image("mcard1").renderingMode(.original)
+                            Text("Hiking").foregroundColor(.gray)
+                        }
+                    }
+                    
+                    Button(action: {
+                            
+                    }) {
+                            
+                        VStack(spacing: 8){
+                                
+                            Image("mcard2").renderingMode(.original)
+                            Text("Sky Diving").foregroundColor(.gray)
+                        }
+                    }
+                        
+                    Button(action: {
+                            
+                    }) {
+                            
+                        VStack(spacing: 8){
+                                
+                            Image("mcard3").renderingMode(.original)
+                            Text("Skiing").foregroundColor(.gray)
+                        }
+                    }
+                    Button(action: {
+                            
+                    }) {
+                            
+                        VStack(spacing: 8){
+                                
+                            Image("mcard4").renderingMode(.original)
+                            Text("SkateBoarding").foregroundColor(.gray)
+                        }
+                    }
+                }
+            }.padding(.leading, 20)
+            .padding([.top,.bottom], 15)
+        }
+    }
+}
+
+struct Rounded : Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [.topLeft,.topRight], cornerRadii: CGSize(width: 40, height: 40))
+        return Path(path.cgPath)
     }
 }
---
+++ SwiftUIDemo/SwiftUIDemo/DetailView.swift
@@ -0,0 +1,150 @@
+
+//
+//  DetailView.swift
+//  SwiftUIDemo
+//
+//  Created by Chen Chen on 11/4/20.
+//  Copyright © 2020 chen chen. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body : some View{
+        VStack{
+            Image("background").resizable().aspectRatio(1.35, contentMode: .fill).frame(width:UIScreen.main.bounds.width,height: 500).offset(y: -200).padding(.bottom, -200)
+            
+            GeometryReader{geo in
+                
+                VStack(alignment: .leading){
+                    
+                   detailTop()
+                   detailMiddle()
+                   detailBottom()
+                    
+                }
+                
+            }.background(Color.white)
+            .clipShape(Rounded())
+            .padding(.top, -75)
+            
+        }
+    }
+}
+
+struct detailTop : View {
+    
+    var body : some View{
+        
+        VStack(alignment: .leading, spacing: 10){
+            
+            HStack{
+                
+                VStack(alignment: .leading){
+                    
+                    Text("Mountain").fontWeight(.heavy).font(.largeTitle)
+                    Text("Camping").fontWeight(.heavy).font(.largeTitle)
+                    
+                }
+                
+                Spacer()
+                
+                Text("$299").foregroundColor(Color("bg")).font(.largeTitle)
+            }
+            
+        }.padding()
+    }
+}
+
+
+struct detailMiddle : View {
+    
+    var body : some View{
+        
+        VStack(alignment: .leading, spacing: 15){
+            
+
+            HStack(spacing: 5){
+                
+                Image("map").renderingMode(.original)
+                Text("Yosemite, CA").foregroundColor(Color("bg"))
+                
+            }
+            
+            HStack(spacing : 8){
+                
+                ForEach(0..<5){_ in
+                    
+                    Image(systemName: "star.fill").font(.body).foregroundColor(.yellow)
+                }
+            }
+            
+            Text("Group size").fontWeight(.heavy)
+            
+            Text("Number Of People In Your Group").foregroundColor(.gray)
+            
+            HStack(spacing: 6){
+                
+                ForEach(0..<5){i in
+                    
+                    Button(action: {
+                        
+                    }) {
+                        
+                        Text("\(i + 1)").foregroundColor(.white).padding(20)
+                        
+                    }.background(Color("bg"))
+                    .cornerRadius(8)
+                }
+            }
+            
+            
+        }.padding(.horizontal,15)
+    }
+}
+
+struct detailBottom : View {
+    
+    var body : some View{
+        
+        VStack(alignment: .leading, spacing: 10){
+            
+            Text("Description").fontWeight(.heavy)
+            Text("Mountain Camping Experiences and Meanings Key elements of camping experience include nature, social interaction, and comfort/convenience. The most common associated meanings are restoration, family functioning").foregroundColor(.gray)
+            
+            HStack(spacing: 8){
+                
+                Button(action: {
+                    
+                }) {
+                    
+                    Image("Save").renderingMode(.original)
+                }
+                
+                Button(action: {
+                    
+                }) {
+                    
+                    HStack(spacing: 6){
+                        
+                        Text("Book Your Experience")
+                        Image("arrow").renderingMode(.original)
+                        
+                    }.foregroundColor(.white)
+                    .padding()
+                    
+                }.background(Color("bg"))
+                .cornerRadius(8)
+                
+            }.padding(.top, 6)
+            
+        }.padding()
+    }
+}
+
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
